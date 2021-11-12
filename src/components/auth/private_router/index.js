d0274dc96c624282c9caff8f564e4de7f2a0eaad
Ishtrb <ishiitorben@gmail.com>
2021-11-12 16:46:17 -0300

    :zap: Add private routes

---
+++ src/components/auth/private_router/index.js
@@ -0,0 +1,16 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const privateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) => (
+      localStorage.getItem('user')
+        ? <Component {...props} />
+        : <Redirect to={{ pathname: '/login' }} />
+    )}
+  />
+);
+
+export default privateRoute;
