eae2e741a169660651423f111c637ba57a9d2d24
Ishtrb <ishiitorben@gmail.com>
2021-11-15 15:31:16 -0300

    :zap: Note lists

--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -1,37 +1,73 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable camelcase */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-shadow */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/jsx-curly-brace-presence */
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../styles/notes.scss';
 import { push as Menu } from 'react-burger-menu';
-import { Column, Button } from 'rbx';
+import { Column } from 'rbx';
+import List from './list';
+import NotesService from '../../services/notes';
 
-const Notes = (props) => (
-  <>
-    <Column.Group className="notes" id="notes">
-      <Menu
-        pageWrapId={'notes-editor'}
-        isOpen={props.isOpen}
-        onStateChange={(state) => props.setIsOpen(state.isOpen)}
-        disableAutoFocus
-        outerContainerId={'notes'}
-        customBurgerIcon={false}
-        customCrossIcon={false}
-      >
-        <Column.Group>
-          <Column size={10} offset={1}>
-            Search...
-          </Column>
-        </Column.Group>
-        <p>List...</p>
-      </Menu>
+function Notes(props) {
+  const [notes, setNotes] = useState([]);
+  const [current_note, setCurrentNote] = useState({
+    title: '',
+    body: '',
+    id: '',
+  });
 
-      <Column size={12} className="notes-editor" id="notes-editor">
-        Editor...
-      </Column>
-    </Column.Group>
-  </>
-);
+  async function fetchNotes() {
+    const response = await NotesService.index();
+    if (response.data.length >= 1) {
+      setNotes(response.data.reverse());
+      setCurrentNote(response.data[0]);
+    }
+  }
+
+  const selectNote = (id) => {
+    const note = notes.find((note) => note._id == id);
+    setCurrentNote(note);
+  };
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  return (
+    <>
+      <Column.Group className="notes" id="notes">
+        <Menu
+          pageWrapId={'notes-editor'}
+          isOpen={props.isOpen}
+          onStateChange={(state) => props.setIsOpen(state.isOpen)}
+          disableAutoFocus
+          outerContainerId={'notes'}
+          customBurgerIcon={false}
+          customCrossIcon={false}
+        >
+          <Column.Group>
+            <Column size={10} offset={1}>
+              Search...
+            </Column>
+          </Column.Group>
+          <List
+            notes={notes}
+            selectNote={selectNote}
+            current_note={current_note}
+          />
+        </Menu>
+
+        <Column size={12} className="notes-editor" id="notes-editor">
+          Editor...
+        </Column>
+      </Column.Group>
+    </>
+  );
+}
 
 export default Notes;
--- src/screens/notes/index/index.js
+++ src/screens/notes/index/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import HeaderLogged from '../../../components/header_logged';
 import Notes from '../../../components/notes';
 
