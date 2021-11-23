3c0f282cb21bc011114930964dcbd177365bc1cc
Ishtrb <ishiitorben@gmail.com>
2021-11-23 15:39:59 -0300

    :zap: Update edit screen

--- src/screens/users/edit/index.js
+++ src/screens/users/edit/index.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import {
+  Column, Section, Title, Container, Card, Button,
+} from 'rbx';
+import '../../../styles/users.scss';
+import HeaderLogged from '../../../components/header_logged';
 
-const UsersEditScreen = () => (
+const UserEditScreen = () => (
   <>
-    Hello World
+    <HeaderLogged />
+    <Section size="medium" className="users">
+      <Container>
+        <Column.Group centered className="users-edit">
+          <Column size={4}>
+            <Title size={5} className="has-text-grey has-text-left">
+              Informações Pessoais
+            </Title>
+            <Card>
+              <Card.Content>Users Edit Form...</Card.Content>
+            </Card>
+          </Column>
+        </Column.Group>
+
+        <Column.Group centered className="users-edit">
+          <Column size={4}>
+            <Title size={5} className="has-text-grey has-text-left">
+              Password
+            </Title>
+            <Card>
+              <Card.Content>Users Edit Password Form...</Card.Content>
+            </Card>
+          </Column>
+        </Column.Group>
+        <Column.Group centered>
+          <Column size={4} className="has-text-right">
+            Users Delete Button...
+          </Column>
+        </Column.Group>
+      </Container>
+    </Section>
   </>
 );
 
-export default UsersEditScreen;
+export default UserEditScreen;
